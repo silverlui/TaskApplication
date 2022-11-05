85f5e9206d94374d48125f52f5f3b1b5c6cbc746
silverlui <luis.cabrera1014@gmail.com>
2022-11-04 21:49:11 -0400

    Database context fix for sqlite

--- TaskApplication/Data/TaskApplicationDbContext.cs
+++ TaskApplication/Data/TaskApplicationDbContext.cs
@@ -6,7 +6,10 @@ namespace TaskApplication.Data;
 public class TaskApplicationDbContext : DbContext
 {
 
-    public TaskApplicationDbContext(DbContextOptions<TaskApplicationDbContext> options) : base(options) {}
+    public TaskApplicationDbContext(DbContextOptions<TaskApplicationDbContext> options) : base(options)
+    {
+        Database.EnsureCreated();
+    }
 
     public DbSet<Todo> Todos => Set<Todo>();
 }
---
+++ TaskApplication/Data/Validation/TodoValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using TaskApplication.Data.Models;
+
+namespace TaskApplication.Data.Validation;
+
+public class TodoValidator : AbstractValidator<Todo>
+{
+    public TodoValidator()
+    {
+        RuleFor(todo => todo.Id).NotNull();
+        RuleFor(todo => todo.Description)
+            .NotNull()
+            .NotEmpty()
+            .Length(1, 50)
+            .WithMessage("Description is required");
+        RuleFor(todo => todo.UpdatedAt)
+            .GreaterThanOrEqualTo(todo => todo.CreatedAt);
+    }
+}
